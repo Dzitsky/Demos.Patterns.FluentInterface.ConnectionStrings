5e6ad8f799a22a9415d9d0be4ae49eb26dd2a377
Pavel Dubsky <pavel.dubsky@padutronics.com>
2022-07-06 23:00:41 +0300

    Extract common connection string settings into constants.

--- Source/Demos.Patterns.FluentInterface.ConnectionStrings/Program.cs
+++ Source/Demos.Patterns.FluentInterface.ConnectionStrings/Program.cs
@@ -1,17 +1,22 @@
 ﻿using Demos.Patterns.FluentInterface.ConnectionStrings;
 using System;
 
+const string ServerName = "MSSQLSERVER";
+const string DatabaseName = "Library";
+const string UserName = "JSmith";
+const string Password = "123qwe";
+
 string connectionString1 = new ConnectionStringBuilder()
-    .SetServerName("MSSQLSERVER")
-    .SetDatabaseName("Library")
-    .SetUserName("JSmith")
-    .SetPassword("123qwe")
+    .SetServerName(ServerName)
+    .SetDatabaseName(DatabaseName)
+    .SetUserName(UserName)
+    .SetPassword(Password)
     .Build();
 Console.WriteLine(connectionString1);
 
 string connectionString2 = new ConnectionStringBuilder()
-    .SetServerName("MSSQLSERVER")
-    .SetDatabaseName("Library")
+    .SetServerName(ServerName)
+    .SetDatabaseName(DatabaseName)
     .SetTrustedConnection()
     .Build();
 Console.WriteLine(connectionString2);
